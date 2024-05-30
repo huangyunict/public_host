7c337652eb6550446a75369a5f4ca2af39a794aa
Yun Huang <huangyunict@gmail.com>
2024-05-30 11:39:15 -0700

    add logger_utils.js

---
+++ monkey/logger_utils.js
@@ -0,0 +1,38 @@
+//  Shared classes / methods for Violentmonkey scripts, which may or may not be used in Greasemonkey / Tampermonkey scripts.
+//  version 0.1
+//  2024-06-01
+//  Copyright (c) 2024, Yun Huang, huangyunict@gmail.com
+//  Released under the GPL license:  http://www.gnu.org/copyleft/gpl.html
+//  Reference to this script: https://raw.githubusercontent.com/huangyunict/public_host/main/monkey/logger_utils.js
+// --------------------------------------------------------------------
+
+/**
+ * Utilities for console loggers.
+ */
+class LoggerUtils {
+  /**
+   * Returns the appropriate logging function based on the provided log level.
+   *
+   * @param {string} logLevel The log level for which to retrieve the logging function.
+   * @return {Function} The logging function corresponding to the provided log level.
+   */
+  static getLogger(logLevel) {
+    if (logLevel === 'debug') return console.debug;
+    if (logLevel === 'info') return console.info;
+    if (logLevel === 'warn' || logLevel === 'warning') return console.warn;
+    if (logLevel === 'error') return console.error;
+  
+    // Default to console.log if logLevel is not recognized or just 'log'.
+    return console.log;
+  }
+
+  /**
+   * Logs a message at the specified log level.
+   *
+   * @param {string} logLevel The log level at which to log the message.
+   * @param {...any} args The message or messages to log.
+   */
+  static logMessage(logLevel, ...args) {
+    LoggerUtils.getLogger(logLevel)(...args);
+  } 
+}
